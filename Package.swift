1f2df86019da5e0ca18fbf3e045d082cc2e0b79a
Serg Krivoblotsky <krivoblotsky@macpaw.com>
2021-01-18 13:04:53 +0800

    Format package

--- Package.swift
+++ Package.swift
@@ -5,20 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "NeumorphicView",
-    platforms: [
-            .macOS(.v10_12),    //.v10_10 - .v10_15
-        ],
+    platforms: [.macOS(.v10_12)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NeumorphicView",
             targets: ["NeumorphicView"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        .target(name: "NeumorphicView", dependencies: [], path: "NeumorphicView/NeumorphicView")
-    ]
+    dependencies: [],
+    targets: [.target(name: "NeumorphicView", dependencies: [], path: "NeumorphicView/NeumorphicView")]
 )
